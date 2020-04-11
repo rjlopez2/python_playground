101ba8ad98479b7ddadda3a123efb95d45c2bdaf
Ruben Lopez <rjlopez2@gmail.com>
2020-04-11 23:54:40 +0200

    my first push

---
+++ ex31_1.py
@@ -0,0 +1,25 @@
+print("""Bienvenido al super super super
+divertidisimo juego de la vida de Ruben """)
+print("Introduce tu nombre para empezar")
+user_name = input(">>> ")
+print(f"\n\nBienvenido {user_name} al mejor juego de tu vida")
+print("""Este podria parecer un juego muy aburrido al principio pero...
+
+Tal vez empiece a gustarte cuando te adrentres a el juego...
+.
+.""")
+input(f"""Quieres intentarlo {user_name}?
+
+preisona Enter para continuar o para salir presiona \"CTRL+C or (^C)\".
+
+>>> """ )
+print(f"""A ver Ruben empecemos con la rutina diaria de Ruben
+
+Ruben cuando se levanta de la cama que crees que hace?
+1. Se levanta como soldado y arregla inmediatamente su cama.
+2. Apaga el despertador y duermem otros 5 min más.
+.
+Escribe acontinuacion tu opción""")
+ruben_al_depertar = input(">>> ")
+
+print(f"""Tu opcion ha sido {ruben_al_depertar}, no bad...""")
